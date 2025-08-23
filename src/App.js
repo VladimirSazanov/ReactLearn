e579bfc58a4852157b75b4c910098482fea0dcd8
Volodya <vova.sazanov@mail.ru>
2025-08-23 16:18:12 +0300

    ReactRouterDOM/Постраничная навигация

--- src/App.js
+++ src/App.js
@@ -1,91 +1,19 @@
-import React, { useEffect, useState } from "react";
-/*import Counter from './components/Counter';*/
+import React from "react";
 import "./styles/App.css";
-/*import ClassCounter from './components/ClassCounter';
-import PostItem from './components/PostItem';*/
-import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton/MyButton";
-import PostForm from "./components/PostForm";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/MyModal/MyModal";
-import { usePosts } from "./hooks/usePost";
-import axios from "axios";
-import PostService from "./API/PostService";
-import Loader from "./components/UI/Loader/Loader";
-import { useFetching } from "./hooks/useFetching";
-import { getPagesArray, getPagesCount } from "./utils/pages";
-import Pagination from "./components/UI/pagination/Pagination";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
+import Navbar from "./components/UI/Navbar/Navbar";
+import Error from "./pages/Error";
+import AppRouter from "./components/AppRouter";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  /*Управляемый компонент-компонент, который хранит своё значение в state, часть двухстороннего связывания*/
-  /*Двухстороннее связывание-это когда управляемый компонент отображается в UI*/
-  const [filter, setFilter] = useState({ sort: "", query: "" });
-  const [modal, setModal] = useState(false);
-  const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [page, setPage] = useState(1);
-  const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
-
-  const [fetchPosts, isPostsLoading, postError] = useFetching(
-    async (limit, page) => {
-      const response = await PostService.getAll(limit, page);
-      setPosts(response.data);
-      const totalCount = response.headers["x-total-count"];
-      setTotalPages(getPagesCount(totalCount, limit));
-    },
-  );
-
-  useEffect(() => {
-    fetchPosts(limit, page);
-  }, []);
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  };
-
-  /// Получаем пост из дочернего компонента
-  const removePosts = (post) => {
-    setPosts(posts.filter((p) => p.id !== post.id));
-  };
-
-  const changePage = (page) => {
-    setPage(page);
-    fetchPosts(limit, page);
-  };
-
   return (
-    <div className="App">
-      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
-        Создать пользователя
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      <hr style={{ margin: "15px 0" }} />
-      <PostFilter filter={filter} setFilter={setFilter} />
-      {postError && <h1>Произошла ошибка. {postError}</h1>}
-      {isPostsLoading ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "50px",
-          }}
-        >
-          <Loader />
-        </div>
-      ) : (
-        <PostList
-          remove={removePosts}
-          posts={sortedAndSearchPosts}
-          title="Посты про JS"
-        />
-      )}
-      <Pagination page={page} changePage={changePage} totalPages={totalPages} />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <AppRouter />
+    </BrowserRouter>
   );
 }
-/* remove - название пропса, которое внутри компонента. removePosts - значение, которое туда передано*/
+
 export default App;
